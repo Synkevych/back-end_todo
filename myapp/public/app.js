9a1c798d8418bd72795399b734990ace836b6df5
Synkevych Roman <sunkevu4@gmail.com>
2019-08-29 17:46:14 +0300

    style: formatting <li> and render it

--- myapp/public/app.js
+++ myapp/public/app.js
@@ -1,14 +1,20 @@
-// run script when script is downloaded 
+// run script when script is downloaded
+
+const ul = document.getElementsByClassName('list')[0];
 
 document.addEventListener('DOMContentLoaded', function() {
-	  let XHR = new XMLHttpRequest();
-		XHR.onreadystatechange = function() {
-			if (XHR.readyState == 4 && XHR.status == 200) {
-				let data = JSON.parse(XHR.responseText);
-				console.log("data",data);
-			}
-		};
-		XHR.open('GET', 'http://localhost:3000/api/todos');
-
-		XHR.send();
+	let XHR = new XMLHttpRequest();
+	XHR.onreadystatechange = function() {
+		if (XHR.readyState == 4 && XHR.status == 200) {
+			let data = JSON.parse(XHR.responseText);
+			data.forEach(todo => {
+				let completed = todo.completed ? 'done' : '';
+				let newTodo = `<li class="task ${completed}"> ${todo.name}</li>`;
+				ul.innerHTML += newTodo;
+				console.log(`task: ${todo.name}`);
+			});
+		}
+	};
+	XHR.open('GET', 'http://localhost:3000/api/todos');
+	XHR.send();
 });
